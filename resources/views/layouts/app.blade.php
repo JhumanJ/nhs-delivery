e7282fc485440735a417dc15657cd1f002d33d70
Pauline <mpgxvii@gmail.com>
2016-03-28 18:23:40 +0100

    designv3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-    <meta name="viewport" content="width=device-width, user-scalable=no" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
 
     {{--No mobile zoom--}}
     {{--<meta name="viewport" content="width=device-width, initial-scale=1">--}}
@@ -27,65 +27,137 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 
+    <!-- Sidebar -->
+    <link rel="stylesheet" href="/css/sidebar/css/lib/fontello.css" />
+    <link rel="stylesheet" href="/css/sidebar/css/lib/normalize.css" />
+    <link rel="stylesheet" href="/css/sidebar/css/index.css" />
+    <link rel="stylesheet" href="/css/sidebar/css/sidebar.css" />
+
+
+    <style>
+
+
+        .fa-btn {
+            margin-right: 6px;
+        }
+    </style>
+
 </head>
+
+
 <body id="app-layout">
-    <nav class="navbar navbar-default">
-        <div class="container">
-            <div class="navbar-header">
 
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
 
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
 
-                    NHS Delivery
 
-                </a>
-            </div>
+<nav class="sidebar jsc-sidebar" id="jsi-nav" data-sidebar-options="">
+<ul class="sidebar-list">
+  <!-- Collapsed Hamburger -->
+  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+      <span class="sr-only">Toggle Navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+  </button>
 
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
+  <!-- Branding Image -->
 
-                    @if (!Auth::guest())
-                        <li><a href="{{ url('deliveries') }}">My Deliveries</a></li>
-                        @if (Auth::user()->isStaff())
-                            <li><a href="{{ url('create') }}">Add Delivery</a></li>
-                            <li><a href="{{ url('deliveries-all') }}">Manage Deliveries</a></li>
-                        @endif
-                    @endif
-                </ul>
+</div>
+<nav>
+<div class="collapse navbar-collapse" id="app-navbar-collapse">
+  <!-- Left Side Of Navbar -->
+  <ul class="nav navbar-nav">
 
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
-                    @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
-                    @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->getFullName() }} <span class="caret"></span>
-                            </a>
+    <p></p>
+    <li><a class="navbar-brand home" style="width:70px;height:63px" href="{{ url('/') }}">
+    </a></li><br>
+      @if (!Auth::guest())
+      <li><a class="collect" style="width:70px;height:63px" href="{{ url('deliveries') }}"></a></li><br>
 
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('/profile') }}"><i class="fa fa-btn fa-user"></i>My Profile</a></li>
-                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                            </ul>
-                        </li>
-                    @endif
-                </ul>
-            </div>
-        </div>
-    </nav>
+
+          @if (Auth::user()->isStaff())
+              <li><a class="add" style="width:76px;height:69px" href="{{ url('create') }}"></a></li><br>
+              <li><a class="delete" style="width:76px;height:69px" href="{{ url('deliveries-all') }}"></a></li><br>
+          @endif
+
+          <li><a class="logout" style="width:70px;height:63px" href="{{ url('/logout') }}"></a></li><br>
+      @endif
+  </ul>
+
+</ul>
+</nav>
+
+<div class="wrapper jsc-sidebar-content jsc-sidebar-pulled">
+
+  <nav class="navbar navbar-default" role="navigation">
+
+
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+  </div>
+  <div class="navbar-collapse collapse">
+    <ul class="nav navbar-nav navbar-left">
+      <li><a href="#" class="icon-menu link-menu jsc-sidebar-trigger" style="color:#38B7D1"></a></li>
+
+    </ul>
+
+
+    <ul class="nav navbar-nav navbar-right">
+
+      <!-- Authentication Links -->
+                  @if (Auth::guest())
+                      <li><a href="{{ url('/login') }}">Login</a></li>
+                      <li><a href="{{ url('/register') }}">Register</a></li>
+                  @else
+                      <li class="dropdown">
+                          <a href="#" style="color:#38B7D1" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                              {{ Auth::user()->getFullName() }} <span class="caret"></span> <br>{{ Auth::user()->getEmailforPasswordReset() }}
+                          </a>
+
+
+                          <ul class="dropdown-menu" role="menu">
+                              <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                          </ul>
+
+                      </li>
+
+
+                  @endif
+    </ul>
+  </div>
+</nav>
+
+
 
     @yield('content')
+
+</div>
+
+    		<script src="/css/sidebar/js/lib/jquery.min.js"></script>
+
+    		<script src="/css/sidebar/js/sidebar.js"></script>
+
+    		<script>
+    			$('#jsi-nav').sidebar({
+    				trigger: '.jsc-sidebar-trigger',
+    				scrollbarDisplay: false,
+    				pullCb: function () { console.log('pull'); },
+    				pushCb: function () { console.log('push'); }
+    			});
+
+    			$('#api-push').on('click', function (e) {
+    				e.preventDefault();
+    				$('#jsi-nav').data('sidebar').push();
+    			});
+    			$('#api-pull').on('click', function (e) {
+    				e.preventDefault();
+    				$('#jsi-nav').data('sidebar').pull();
+    			});
+    		</script>
 
 </body>
 </html>
